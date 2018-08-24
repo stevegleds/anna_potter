385d2a7618d6947fefd3e10507213132ddc0cc51
Steve Gledhill <stevegleds@gmail.com>
2018-08-24 14:04:28 +0100

    24 August 2018 Day 2 class methods

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ class CastleKilmereMember:
     def says(self, words):
         return f"{self._name} says {words}"
 
+    @staticmethod
+    def school_headmaster():
+        return CastleKilmereMember('Redmond Dalodore', 1939, 'male')
+
 
 class Pupil(CastleKilmereMember):
     """
@@ -42,17 +46,94 @@ class Pupil(CastleKilmereMember):
             'Transfiguration': False}
         # todo why _elms not elms ?
 
+    @staticmethod
+    def passed(grade):
+        """
+        Given a grade, determine if an exam was passed
+        :param grade: the grade achieved by the pupil
+        :return: True if grade was enough to pass or else False
+        Returns False if grade not found
+        """
+        grades = {
+            'O': True,
+            'Ordinary': True,
+            'P': True,
+            'Passed': True,
+            'A': True,
+            'Acceptable': True,
+            'P': False,
+            'Poor': False,
+            'H': False,
+            'Horrible': False,
+        }
+        return grades.get(grade, False)
 
-bromley = CastleKilmereMember('Bromley Huckabee', '1959', 'male')
-cleon = Pupil(name='Cleon Bery',
-              birthyear=2008,
-              sex='male',
-              house='House of Courage',
-              start_year=2018,
-              pet=('Cotton', 'owl'))
+    @classmethod
+    def cleon(cls):
+        return cls('Cleon Bery', 2008, 'male', 'House of Courage', 2018, ('Cotton', 'owl'))
+
+    @classmethod
+    def flynn(cls):
+        return cls('Flynn Gibbs', 2008, 'male', 'House of Courage', 2018, ('Twiggles', 'owl'))
+
+    @classmethod
+    def cassidy(cls):
+        return cls('Cassidy Ambergem', 2007, 'female', 'House of Courage', 2018, ('Ramses', 'cat'))
+
+
+class Professor(CastleKilmereMember):
+    """
+    Creates a Castle Kilmere professor
+    """
+
+    def __init__(self, name, birthyear, sex, subject, house=None):
+        super().__init__(name, birthyear, sex)
+        self.subject = subject
+        if house is not None:
+            self.house = house
+
+    @classmethod
+    def mirren(cls):
+        return cls('Miranda Mirren', 1963, 'female', 'Transfiguration', 'House of Courage')
+
+    @classmethod
+    def blade(cls):
+        return cls('Blade Bardock', 1988, 'male', 'Potions', 'House of Ambition')
+
+
+class Ghost(CastleKilmereMember):
+    """
+    Creates a Castle Kilmere ghost
+    """
+
+    def __init__(self, name, birthyear, sex, year_of_death, house=None):
+        super().__init__(name, birthyear, sex)
+        self.year_of_death = year_of_death
+
+        if house is not None:
+            self.house = house
+
+
+if __name__ == "__main__":
+    bromley = CastleKilmereMember('Bromley Huckabee', '1959', 'male')
+
+    mirren = Professor.mirren()
+    blade = Professor.blade()
+    cleon = Pupil.cleon()
+    flynn = Pupil.flynn()
+    cassidy = Pupil.cassidy()
+
+
+# cleon = Pupil(name='Cleon Bery',
+#               birthyear=2008,
+#               sex='male',
+#               house='House of Courage',
+#               start_year=2018,
+#               pet=('Cotton', 'owl'))
 
 print(bromley.says("Hello"))
-print(cleon)
+print(cleon.pet_name, cleon.pet_type)
+
 print(cleon.location)
 print(cleon.birthyear, cleon.start_year)
 print(CastleKilmereMember.location)
